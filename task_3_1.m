9afdcb2529be46ac73bc824ddfe2eda5eec44011
stevespreiz <65625597+stevespreiz@users.noreply.github.com>
2020-06-01 15:49:54 -0400

    Add files via upload
    Driver code for testing up to 4th order accuracy

---
+++ task_3_1.m
@@ -0,0 +1,28 @@
+% 4th order accurate method using exact soln for ghost points and first
+% 2 time steps for now
+
+def.a = 0;
+def.b = 1;
+def.N = 100;
+def.c = 1;
+tf = 3.15;
+def.f = @(x) sin(3*pi/2*x);
+def.g = @(x) 0;
+def.l = @(t) 0;
+sigma = .9;
+y = @(x,t) 0.5*(def.f(x-def.c*t)+def.f(x+def.c*t));
+
+[u,e] = oneDSolver4(def,tf,sigma,1,4);
+
+for n = [10 100 1000 10000]
+   def.N = n;
+   [u,er] = oneDSolver4(def, tf, sigma, 1,4);
+   e(log10(n)) = er;
+end
+
+h = [.1 .01 .001 .0001];
+h2 = h.^2;
+h4 = h.^4;
+figure
+loglog(h,e,h, h2,h,h4)
+legend('error', 'h^2', 'h^4')
