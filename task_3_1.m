581e0c86c3c58f286cf0227d14a78ffe4e087610
stevespreiz <65625597+stevespreiz@users.noreply.github.com>
2020-06-02 16:29:39 -0400

    Add files via upload

--- task_3_1.m
+++ task_3_1.m
@@ -7,22 +7,22 @@ def.N = 100;
 def.c = 1;
 tf = 3.15;
 def.f = @(x) sin(3*pi/2*x);
-def.g = @(x) 0;
+def.g = @(x) -sin(3*pi/2*x);
 def.l = @(t) 0;
 sigma = .9;
 y = @(x,t) 0.5*(def.f(x-def.c*t)+def.f(x+def.c*t));
 
-[u,e] = oneDSolver4(def,tf,sigma,1,4);
+% [u,e] = oneDSolver4(def,tf,sigma,1,4);
 
-for n = [10 100 1000 10000]
+for n = [10 100 1000]
    def.N = n;
    [u,er] = oneDSolver4(def, tf, sigma, 1,4);
    e(log10(n)) = er;
 end
 
-h = [.1 .01 .001 .0001];
+h = [.1 .01 .001 ];
 h2 = h.^2;
 h4 = h.^4;
 figure
-loglog(h,e,h, h2,h,h4)
+loglog(h,e,'o',h, h2,h,h4)
 legend('error', 'h^2', 'h^4')
