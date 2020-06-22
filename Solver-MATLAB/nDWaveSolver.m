e6720bbc4f3037cefda108e62126a1683009c8a5
stevespreiz <stevespreiz@users.noreply.github.com>
2020-06-22 17:18:26 -0400

    debugging 6th order time step

--- Solver-MATLAB/nDWaveSolver.m
+++ Solver-MATLAB/nDWaveSolver.m
@@ -13,6 +13,7 @@ function [u,e] = nDWaveSolver(def,sigma,tf,nD,icase,oacc)
 %       B.C. u(a,t) = l(t), u(b,t) = r(t)
 %       I.C. u(x,0) = f(x), u_t(x,0) = g(x)
 %   oacc       - order of accuracy (2 or 4)
+ys = @(x,t) 0.5*(def.f(x-def.c*t)+def.f(x+def.c*t)+ integral(def.g,x-def.c*t,x+def.c*t)/def.c);
 
 % Setup
 [sigma,x,dt,unm1,un,unp1,ja,jb] = setup(def,sigma,tf,nD,oacc);
@@ -21,9 +22,14 @@ function [u,e] = nDWaveSolver(def,sigma,tf,nD,icase,oacc)
 unm1 = IC(def,x,unm1);
 
 % First Time Step
-un = firstStep(def,sigma,x,dt,ja,jb,unm1,un,nD,oacc);
-un = BC(def,sigma,x,1,dt,ja,jb,un,nD,icase,oacc);
-% pause
+% un = firstStep(def,sigma,x,dt,ja,jb,unm1,un,nD,oacc);
+% un = BC(def,sigma,x,1,dt,ja,jb,un,nD,icase,oacc);
+
+for i = 1:def.N+1+oacc
+   un(i) = ys(x(i),dt); 
+end
+
+
 % Remaining Time Steps
 n = 2;
 while n*dt <= tf
@@ -31,13 +37,19 @@ while n*dt <= tf
     unp1 = timeStep(sigma,ja,jb,unm1,un,unp1,nD,oacc);
     
     % Apply Boundary Condition
-    unp1 = BC(def,sigma,x,n,dt,ja,jb,unp1,nD,icase,oacc);
+%     unp1 = BC(def,sigma,x,n,dt,ja,jb,unp1,nD,icase,oacc);
+    unp1(ja-3) = ys(x(ja-3),n*dt);
+    unp1(ja-2) = ys(x(ja-2),n*dt);
+    unp1(ja-1) = ys(x(ja-1),n*dt);
+    unp1(jb+1) = ys(x(jb+1),n*dt);
+    unp1(jb+2) = ys(x(jb+2),n*dt);
+    unp1(jb+3) = ys(x(jb+3),n*dt);
     
     % Optional Animation
 %     plot(x,unp1);
 %     ylim([-1.1 1.1])
 %     xlim([-.1 1.1])
-%     pause;
+%     pause(.1);
     
     % Update arrays
     unm1 = un;
@@ -49,10 +61,10 @@ end
 e = 0;
 if icase == 1
     % Error calc with exact solution for case 1
-    y = @(x,t) 0.5*(def.f(x-def.c*t)+def.f(x+def.c*t)+ integral(def.g,x-def.c*t,x+def.c*t)/def.c);
+    ys = @(x,t) 0.5*(def.f(x-def.c*t)+def.f(x+def.c*t)+ integral(def.g,x-def.c*t,x+def.c*t)/def.c);
 
     for i = ja:jb
-        e = max(e,abs(un(i)-y(x(i),tf)));
+        e = max(e,abs(un(i)-ys(x(i),tf)));
     end
 end
 
