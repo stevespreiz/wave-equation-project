37970e8a1760e4eb3dc4576d974b2478f383169d
stevespreiz <stevespreiz@users.noreply.github.com>
2020-06-23 20:06:33 -0400

    Update nDWaveSolver.cpp

--- Solver-C++/nDWaveSolver.cpp
+++ Solver-C++/nDWaveSolver.cpp
@@ -1,6 +1,6 @@
 /* n-Dimensional Wave Equation Solver
 *
-*  Currently being built to support 1D then 2
+*  Currently being built to support 1D then 2D
 *
 */
 
@@ -46,6 +46,25 @@ struct Definition {
   R r;
 };
 
+void firstStep(Definition* def, double sigma, double* x, double dt, int ja, int jb,
+        double* unm1, double* un, int nD, int oacc){
+  if(nD ==  1){
+    for(int i = ja; i<=jb; i++){
+      un[i] = unm1[i]
+            + dt*def->g(x[i])
+            + pow(sigma,2)*(unm1[i+1]-2*unm1[i]+unm1[i-1]);
+      if(oacc > 2){
+        un[i] += -1*pow(sigma,2)*(unm1[i+2]-4*unm1[i+1]+6*unm1[i]-4*unm1[i-1]+unm1[i-2])/12
+               + dt*pow(sigma,2)/6*( (def->g(x[i+1])-2*def->g(x[i])+def->g(x[i-1])) - (def->g(x[i+2])-4*def->g(x[i+1])+6*def->g(x[i])-4*def->g(x[i-1])+def->g(x[i-2]))/12)
+               + pow(sigma,4)/24*(unm1[i+2]-4*unm1[i+1]+6*unm1[i]-4*unm1[i-1]+unm1[i-2]);
+        if(oacc > 4){
+          // this is a long one
+        }
+      }
+    }
+  }
+}
+
 void timeStep(double sigma, int ja, int jb, double* unm1, double* un, double* unp1,
         int nD, int oacc){
   if(nD == 1){
@@ -53,6 +72,9 @@ void timeStep(double sigma, int ja, int jb, double* unm1, double* un, double* un
       unp1[i] = 2*un[i] - unm1[i] + pow(sigma,2)*(un[i+1]-2*un[i]+un[i-1]);
       if(oacc > 2){
         unp1[i] += (pow(sigma,2)-pow(sigma,4))/12*(un[i+2]-4*un[i+1]+6*un[i]-4*un[i-1]+un[i-2]);
+        if(oacc > 4){
+          unp1[i] += (pow(sigma,2)/90-pow(sigma,4)/72+pow(sigma,6)/720)*(un[i+3]-6*un[i+2]+15*un[i+1]-20*un[i]+15*un[i-1]-6*un[i-2]+un[i-3]);
+        }
       }
     }
   }
@@ -60,19 +82,61 @@ void timeStep(double sigma, int ja, int jb, double* unm1, double* un, double* un
 
 void BC(Definition* def, double sigma, double* x, int n, double dt, int ja, int jb,
         double* unp1, int nD, int iCase, int oacc){
+  double dx = x[1]-x[0];
   if(nD == 1){
-    // Dirchlet left, Neumann right
+    // Dirchlet left (assume l_tt = 0), Neumann right
     if(iCase == 1){
       if(oacc == 2){
-        unp1[jb+1] = unp1[jb-1];
+        unp1[jb+1] = unp1[jb-1] + 2*dx*def->r(n*dt);
         unp1[ja-1] = 2*unp1[ja]-unp1[ja+1];
       }
+      else if(oacc == 4){
+        // Left hand Dirchlet
+        // inline double* vecFunc(double u1, double u2){
+        //   double* ret = new double[2];
+        //   ret[1] = pow(def->c,2)/pow(dx,2)*(u1-2*unp1[ja]+unp1[ja+1] - (u2-4*u1+6*unp1[ja]-4*unp1[ja+1]+unp1[ja+2])/12);
+        //   ret[2] = pow(def->c,2)/pow(dx,4)*(u2-4*u1+6*unp1[ja]-4*unp1[ja+1]+unp1[ja+2]);
+        //   return ret;
+        // };
+        // double* f0 = vecFunc(0,0);
+        // double* f1 = vecFunc(1,0);
+        // double* f2 = vecFunc(0,1);
+        //
+        // double** A = new double*[2];
+        // A[0] = new double[2];
+        // A[1] = new double[2];
+        // A[0][0] = f1[0]-f0[0];
+        // A[1][0] = f1[1]-f0[0];
+        // A[0][1] = f2[0]-f0[0];
+        // A[1][1] = f2[1]-f0[1];
+        // double *b = new double[2];
+        // b[0] = -1*f0[0];
+        // b[1] = -1*f0[1];
+
+        // Right hand Neumann
+      }
+      else if(oacc == 6){
+        // Left hand Dirchlet
+
+        // Right hand Neumann
+      }
     }
-    // Dirchlet both sides
+    // Dirchlet both sides (assume l_tt = r_tt = 0)
     if(iCase == 2){
       if(oacc == 2){
         unp1[ja-1] = 2*unp1[ja]-unp1[ja+1];
         unp1[jb+1] = 2*unp1[jb]-unp1[jb-1];
+      }
+      else if(oacc == 4){
+        // Left hand Dirchlet
+
+        // Right hand Dirchlet
+      }
+      else if(oacc == 6){
+        // Left hand Dirchlet
+
+        // Right hand Dirchlet
+
       }
     }
   }
@@ -154,21 +218,8 @@ int main(int argc, char* argv[]){
 
   /////////////////////////////////////////////////////////////////////////////
   //  First Time Step
-  if(nD == 1){
-    if(oacc == 2){
-      for(int i = ja; i <= jb; i++){
-        un[i] = (1-pow(sigma,2))*unm1[i] + dt*def->g(x[i])+pow(sigma,2)/2*(unm1[i-1]+unm1[i+1]);
 
-      }
-    }
-    else if(oacc == 4){
-      for(int i = ja; i<= jb; i++){
-
-      }
-    }
-  }
-
-
+  firstStep(def, sigma, x, dt, ja, jb, unm1, un, nD, oacc);
   BC(def,sigma,x,1,dt,ja,jb,un,nD,icase,oacc);
 
   for(int i = 0; i < arrSize ; i++)
@@ -184,7 +235,7 @@ int main(int argc, char* argv[]){
     // Update boundary conditions
     BC(def,sigma,x,n,dt,ja,jb,unp1,nD,icase,oacc);
 
-    // Update array pointers
+    // Update arrays
     for(int i = 0; i<arrSize; i++){
       unm1[i] = un[i];
       un[i] = unp1[i];
