b705a3d4587c2bb4822d5de335cff1fc6e059f23
stevespreiz <stevespreiz@users.noreply.github.com>
2020-07-09 17:30:18 -0400

    testing

--- Solver-C++/plotting.py
+++ Solver-C++/plotting.py
@@ -4,11 +4,11 @@ from matplotlib import pyplot as plt
 
 f = open("out.txt")
 
-x = np.array((f.readline().split("\t"))[2:-3],dtype = float)
-u = np.array((f.readline().split("\t"))[2:-3],dtype = float)
+x = np.array((f.readline().split("\t"))[3:-4],dtype = float)
+u = np.array((f.readline().split("\t"))[3:-4],dtype = float)
 
 for i in range(20):
-    u = np.append(u,np.array((f.readline().split("\t"))[2:-3],dtype = float),axis = 0)
+    u = np.append(u,np.array((f.readline().split("\t"))[3:-4],dtype = float),axis = 0)
 u = np.reshape(u, (21,11))
 
 plt.plot(x,u[0,:])
